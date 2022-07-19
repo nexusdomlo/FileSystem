8f0d51e8f5de419d68de10d1cac73efc2cfda9ca
shenxm <2392224351@qq.com>
2022-07-19 20:39:08 +0800

    沈晓敏 2021/7/19 第二次提交

--- JavaProject/FileSystem/src/main/java/com/example/filesystem/HelloController.java
+++ JavaProject/FileSystem/src/main/java/com/example/filesystem/HelloController.java
@@ -2,8 +2,36 @@ package com.example.filesystem;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TreeView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 public class HelloController {
+    @FXML
+    private ImageView AboutUs;
+
+    @FXML
+    private ImageView FileStorage;
+
+    @FXML
+    private ImageView StartMenu;
+
+    @FXML
+    private Pane StoragePane;
+
+    @FXML
+    private ImageView Tips;
+
+    @FXML
+    private javafx.scene.control.TreeView<?> TreeView;
+
+    @FXML
+    private AnchorPane mainInterface;
+
+    @FXML
+    private ImageView main_BG;
+
     @FXML
     private Label welcomeText;
 
