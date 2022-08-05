65131b59aade4819a66dc86d0eaf0750a94d6f07
shenxm <2392224351@qq.com>
2022-08-05 14:50:46 +0800

    沈晓敏 2022/8/5 第1次提交

--- JavaProject/FileSystem/src/main/java/com/example/filesystem/HelloController.java
+++ JavaProject/FileSystem/src/main/java/com/example/filesystem/HelloController.java
@@ -1,10 +1,12 @@
 package com.example.filesystem;
 
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.geometry.Side;
 import javafx.scene.Group;
@@ -16,9 +18,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -93,16 +93,7 @@ public class HelloController {
 //    }
 
 
-//    public void init(){
-//        ObservableList<PieChart.Data> pieChartData =
-//                FXCollections.observableArrayList(
-//                        new PieChart.Data("Grapefruit", 13),
-//                        new PieChart.Data("Oranges", 25),
-//                        new PieChart.Data("Plums", 10),
-//                        new PieChart.Data("Pears", 22),
-//                        new PieChart.Data("Apples", 30));
-//        pieChart.setData(pieChartData);
-//    }
+
 
 //    Tooltip tooltip = new Tooltip("帮助");
 //    tooltip.setFont(new Font("YouYuan", 16));
@@ -112,27 +103,53 @@ public class HelloController {
     @FXML
     void initIllustrate(MouseEvent event) {
         //滚动面板
-       ScrollPane sp = new ScrollPane();
-        //纵向滚动条
-        sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-        TextArea textarea = new TextArea("命令类型：操作类型 目录名\\文件名\n          默认最初的目录为根目录\n          命令中，最后的指令名字前带$指类型是目录 例如 create a\\$b 在a目录下创建b目录\n                                                  create a\\b 在目录下创建b文件 \n          改变文件属性中OR为只读 RAW为读与写 例如 change a.OR 改为只读 \n                                                  change a.RAW 改为读与写 \n                                 创建出来的文件默认是读与写 不支持文件夹属性改变 \n创建命令：create \n          例子 创建文件：create a        在b文件夹中创建文件 create b\\a \n            创建文件夹：create $c       在文件夹中创建文件夹 create b\\$c \n打开命令：open\n          例子 打开文件：open a        打开文件夹下的文件 open b\\a\n            打开文件夹：open $c       打开文件夹下的文件夹 open b\\$c\n\n关闭命令：close\n          例子 关闭文件：close a       关闭文件夹下的文件 close b\\a\n            关闭文件夹：close $c      关闭文件夹下的文件夹 close b\\$c\n\n以下命令列子相似\n\n删除命令：delete\n\n查询命令：search\n\n改变文件属性命令（只支持文件类型）：change\n                                    例如 change a.OR 改为只读\n                                        change a.RAW 改为读与写 \n\t\t\t\t创建出来的文件默认是读与写 不支持文件夹属性改变\n");
-        //自动换行
-        textarea.setWrapText(true);
-
-        textarea. setEditable(false);
-        //sp.getChildren().add(textarea);
-        Scene scene = new Scene(sp);
-
+        VBox vb = new VBox();
+        ScrollBar sc = new ScrollBar();
+        Group gp = new Group();
+        Scene scene = new Scene(gp,550,450);
         Stage helpStage = new Stage();
         helpStage.setScene(scene);
-
         helpStage.setTitle("帮助");
-        helpStage.setHeight(450);
-        helpStage.setWidth(550);
         //窗体大小不可拉伸
         helpStage.setResizable(false);
-        helpStage.show();
+        gp.getChildren().addAll(sc,vb);
 
+        //设置VBox布局器的水平方向左侧布局位置，5个像素
+        vb.setLayoutX(5);
+        vb.setSpacing(10);
+
+        sc.setLayoutX(scene.getWidth() - sc.getWidth());
+        sc.setMin(0);
+        sc.setOrientation(Orientation.VERTICAL);
+        //ScrollBar对象的初始化高度
+        sc.setPrefHeight(450);
+        //ScrollBar对象的活动位置最大值
+        sc.setMax(360);
+
+        sc.setUnitIncrement(10.0);
+        sc.setBlockIncrement(5.0);
+
+        TextArea text = new TextArea("命令类型：操作类型 目录名\\文件名\n          默认最初的目录为根目录\n          命令中，最后的指令名字前带$指类型是目录 例如 create a\\$b 在a目录下创建b目录\n                                                  create a\\b 在目录下创建b文件 \n          改变文件属性中OR为只读 RAW为读与写 例如 change a.OR 改为只读 \n                                                  change a.RAW 改为读与写 \n                                 创建出来的文件默认是读与写 不支持文件夹属性改变 \n创建命令：create \n          例子 创建文件：create a        在b文件夹中创建文件 create b\\a \n            创建文件夹：create $c       在文件夹中创建文件夹 create b\\$c \n打开命令：open\n          例子 打开文件：open a        打开文件夹下的文件 open b\\a\n            打开文件夹：open $c       打开文件夹下的文件夹 open b\\$c\n\n关闭命令：close\n          例子 关闭文件：close a       关闭文件夹下的文件 close b\\a\n            关闭文件夹：close $c      关闭文件夹下的文件夹 close b\\$c\n\n以下命令列子相似\n\n删除命令：delete\n\n查询命令：search\n\n改变文件属性命令（只支持文件类型）：change\n                                    例如 change a.OR 改为只读\n                                        change a.RAW 改为读与写 \n\t\t\t\t创建出来的文件默认是读与写 不支持文件夹属性改变\n");
+        gp.getChildren().add(text);
+
+//         //ScrollBar对象进行改变值事件监听处理机制
+//        sc.valueProperty().addListener((ObservableValue<? extends Number> ov,
+//                                        Number old_val, Number new_val) -> {
+//            vb.setLayoutY(-new_val.doubleValue());
+//        });
+
+         //为垂直VBOX实现滚动
+//        sc.valueProperty().addListener((ObservableValue<? extends Number> ov,
+//                                        Number old_val, Number new_val) -> {
+//            vb.setLayoutY(-new_val.doubleValue());
+//        });
+          sc.valueProperty().addListener((ov, old_val, new_val) -> {
+              vb.setLayoutY(-new_val.doubleValue());
+          });
+
+
+
+        helpStage.show();
     }
 
     //关于我们按钮点击事件
@@ -161,7 +178,7 @@ public class HelloController {
         aboutStage.show();
     }
 
-    //文件系统按钮
+    //存储按钮
     @FXML
     void Store(MouseEvent event) {
         String filename = new String();
@@ -176,7 +193,7 @@ public class HelloController {
         commandPane.getChildren().add(tree);
 
         //单行输入框
-          //Pane hbPane = new Pane();
+        Pane hbPane = new Pane();
         HBox hbox = new HBox();
         Label label = new Label("ROOT:>");
         //label.setPadding(15,15,20,20);
@@ -194,33 +211,30 @@ public class HelloController {
          //设置单行输入框的推荐列数
         field.setPrefColumnCount(11);
         hbox.getChildren().addAll(label,field);
-        commandPane.getChildren().add(hbox);
-          //Scene hbScene = new Scene(hbPane);
+        hbPane.getChildren().add(hbox);
+        commandPane.getChildren().add(hbPane);
 
         //饼图
         ObservableList<PieChart.Data> pieChartData =
                 FXCollections.observableArrayList(
-                        new PieChart.Data("沈阳",13),
-                        new PieChart.Data("鞍山",25),
-                        new PieChart.Data("盘锦",10),
-                        new PieChart.Data("大连",22),
-                        new PieChart.Data("葫芦岛",30));
+                        new PieChart.Data("沈阳",50),
+                        new PieChart.Data("鞍山",50));
         PieChart pieChart = new PieChart(pieChartData);
-        pieChart.setTitle("资源分布");
-        //Group group = new Group();
-        //group.getChildren().add(pieChart);
-        Scene s1 = new Scene(new Group());
-        ((Group) s1.getRoot()).getChildren().add(pieChart);
-
-        Scene cscene = new Scene(commandPane,500,500);
+        pieChart.setTitle("磁盘资源使用情况");
+        //设置标签可见
+        pieChart.setLabelsVisible(true);
+        Pane p =new Pane();
+        p.getChildren().add(pieChart);
+        p.setLayoutX(100);
+        p.setLayoutY(100);
+        commandPane.getChildren().add(p);
+        Scene scene = new Scene(commandPane,500,500);
 
         addRightMenu(tree);
         Stage startStage = new Stage();
-        startStage.setScene(cscene);
-        startStage.setScene(s1);
-          //startStage.setScene(hbScene);
+        startStage.setScene(scene);
         startStage.setResizable(false);
-        startStage.setTitle("文件系统");
+        startStage.setTitle("存储");
         startStage.show();
 
     }
